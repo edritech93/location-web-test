01e986ed2a6cd2fb6cc92ace807075bbe346f875
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-09-10 14:58:58 +0700

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
+  const [isSupport, setIsSupport] = useState(false);
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
 
@@ -10,23 +10,14 @@ function App(props) {
     navigator.geolocation.getCurrentPosition(function (position) {
       setLatitude(position.coords.latitude)
       setLongitude(position.coords.longitude)
+      setIsSupport(true)
     });
   }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <text>{`Is Support Location: ${isSupport}`}</text>
         <text>{`Latitude: ${latitude}`}</text>
         <text>{`Longitude: ${longitude}`}</text>
       </header>
